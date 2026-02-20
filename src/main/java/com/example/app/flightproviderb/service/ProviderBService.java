e0298e2c3552556f6f7a141149634defe1a5d709
aa-cmd <goetherwerther94@gmail.com>
2026-02-20 12:50:43 +0300

    predefined classes added for flight search

---
+++ src/main/java/com/example/app/flightproviderb/service/ProviderBService.java
@@ -0,0 +1,28 @@
+package com.example.app.flightproviderb.service;
+
+import com.example.app.flightproviderb.flightdetails.AirFaultMessage;
+import com.example.app.flightproviderb.flightdetails.FlightInformationReq;
+import com.example.app.flightproviderb.flightdetails.FlightService;
+import jakarta.xml.ws.WebServiceFeature;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProviderBService {
+    public String flightsAvailable(String origin, String destination, LocalDateTime departureDate) throws AirFaultMessage {
+
+        var service = new FlightService();
+
+        FlightInformationReq req = new FlightInformationReq();
+
+
+        var infoPort = service.getFlightInfoPort();
+        var service2 = infoPort.service(req);
+        var infoList = service2.getFlightInfo();
+
+        service.getFlightDetailsPort();
+        return "flights available";
+    }
+
+}
