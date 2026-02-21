215b3b242812f7b1f47f506e3335a6db1f126d1e
aa-cmd <goetherwerther94@gmail.com>
2026-02-21 08:06:51 +0300

    LocalDateTimeAdapter issue figured out

--- src/main/java/com/example/app/flightproviderb/util/LocalDateTimeAdapter.java
+++ src/main/java/com/example/app/flightproviderb/util/LocalDateTimeAdapter.java
@@ -6,7 +6,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+//    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     @Override
     public LocalDateTime unmarshal(String s) throws Exception {
